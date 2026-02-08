5e0a9be87f6adbbab8986bb904ed8a0edaa7b657
Fatih Mahawisesa <222011585@stis.ac.id>
2026-02-08 14:12:22 +0800

    test push

--- Modules/Umum/resources/js/pages/UmumPage.vue
+++ Modules/Umum/resources/js/pages/UmumPage.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import { media } from '@/lib/media';
 import UmumComponent from '../components/UmumComponent.vue';
-
 </script>
 
 <template>
     <Button class="w-full transform rounded-md" as-child>
-        <a href="/"> Ini umum sih, aman sejauh ini ya wkwk </a>
+        <a href="/"> Ini page umum</a>
     </Button>
 
+    <img :src="media + 'img/logo.png'" />
     <UmumComponent />
 </template>
